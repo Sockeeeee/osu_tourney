cc5f95ddf94a8c82c427aa87bb78af6d62e3a396
Felix Frankenfeld <franken1@uni-bremen.de>
2025-01-28 08:50:15 +0100

    Redeploy web app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0b3462fb3ee76fdfd60ba5ca9934264
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "24c12059827be61b215da25ff199c389",
+"flutter_bootstrap.js": "bc27d7724e004b2e99bed6125b2f444a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "67cd9dab087fb11fc18474d50e8d0779",
 "/": "67cd9dab087fb11fc18474d50e8d0779",
-"main.dart.js": "e2ae53e3952feeca39cf992f5e46f8e6",
+"main.dart.js": "dad532b8bd167ad3ce1aa12a8c483df7",
 "manifest.json": "abd8272ff9e6c675dad56430342b8cfe",
 "version.json": "a63af0ec5a2e0ecbc05ca43f93fabd7a"};
 // The application shell files that are downloaded before a service worker can
